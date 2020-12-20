dbb2b84ab0f3e75b1fefb16c31ac73e377fa4991
Viktor Gazsi <Beltorion@users.noreply.github.com>
2020-12-20 07:12:15 +0100

    second commit

--- src/com/beltorion/Main.java
+++ src/com/beltorion/Main.java
@@ -3,6 +3,6 @@ package com.beltorion;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Wanderer");
+        System.out.println("Hello Wanderer again");
     }
 }
