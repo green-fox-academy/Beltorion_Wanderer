5d723e5448a3b94bf2bda9c2156c12b61132c00d
Viktor Gazsi <Beltorion@users.noreply.github.com>
2020-12-30 06:53:41 +0100

    Thread and game controler methods has been added to the GameControler class

--- src/com/beltorion/wanderer/Wanderer.java
+++ src/com/beltorion/wanderer/Wanderer.java
@@ -1,13 +1,12 @@
 package com.beltorion.wanderer;
 
-import com.beltorion.wanderer.repositories.Board;
 import com.beltorion.wanderer.repositories.GameControler;
-import com.beltorion.wanderer.repositories.GameMap;
 
 public class Wanderer {
 
     public static void main(String[] args) {
-        new GameControler("wanderer", 720,720);
+        GameControler gameControler = new GameControler("wanderer", 720, 720);
+        gameControler.start();
 
 //        GameMap gameMap=new GameMap();
 //        gameMap.mapLenght();
--- src/com/beltorion/wanderer/repositories/GameControler.java
+++ src/com/beltorion/wanderer/repositories/GameControler.java
@@ -1,14 +1,66 @@
 package com.beltorion.wanderer.repositories;
 
-public class GameControler {
-    private Board board;
+public class GameControler implements Runnable {
 
+    private Board board;
     public int width, height;
+    public String title;
+
+    private boolean running = false;
+    private Thread thread;
 
     public GameControler(String title, int width, int height) {
+        this.title = title;
         this.width = width;
         this.height = height;
-        
+
+    }
+
+    private void init() {
         board = new Board(title, width, height);
+
+    }
+
+    private void upDate() {
+
+    }
+
+    private void render() {
+
+    }
+
+    /* Needs for Runnable interface
+    starts the program, refresh the variables and renders the elements to the screen.
+    At the end it is stopping the program if necessary
+
+     */
+    public void run() {
+        init();
+        while (running) {
+            upDate();
+            render();
+        }
+        stop();
+    }
+
+    //Starts the thread
+    public synchronized void start() {
+        if (running)
+            return;
+        running = true;
+        thread = new Thread(this);
+        thread.start();
+
+    }
+
+    //Stops the thread
+    public synchronized void stop() {
+        if(!running)
+            return;
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
